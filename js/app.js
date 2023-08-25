53d8d8753a9208728e5509723e9ebd22667a8151
Tutul Kabir <tutulkabir@gmail.com>
2023-08-25 20:44:43 +0530

    Pin matcher overview Generate pin and display it

---
+++ js/app.js
@@ -0,0 +1,25 @@
+function getPin() {
+  const pin = generatePin();
+  const pinString = pin + "";
+
+  if (pinString.length === 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
+}
+
+function generatePin() {
+  const random = Math.round(Math.random() * 10000);
+
+  return random;
+}
+
+document.getElementById("generate-pin").addEventListener("click", () => {
+  const pin = getPin();
+
+  // display pin
+
+  const displayPinField = document.getElementById("display-pin");
+  displayPinField.value = pin;
+});
